d793eee4460ab5ed5161537794de216d3a4a6359
EiadSS <eiads.sulaiman@gmail.com>
2024-03-23 20:36:53 -0400

    added snippet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ export default function App() {
     <div className="page">
       <h1>Welcome To FootyFinder</h1>
       <Snippet hideSymbol hideCopyButton variant="solid">Make sure to input the players first name then the last, everything has to be spelled corectly :)</Snippet>
+      <Snippet hideSymbol hideCopyButton variant="solid">Beware the first search may take up to 60s if the server is facing inactivity as it spins down due to low usage. After the first search is complete it will be much faster!</Snippet>
       <Bar setShow={setShow} setPlayer={setPlayer} load={load} setLoad={setLoad} />
       {show &&
         (
